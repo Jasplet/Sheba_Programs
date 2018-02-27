3ca9897d7ba3663600debc38cc47962d4ec5332d
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2018-02-27 13:57:40 +0000

    Added awk implementation to fix "too many files" problem. Also fixed bug with printing of Station codes althought this makes tidyup.sh depedant on tidyup_by_stat.sh to add them in

--- tidyup.sh
+++ tidyup.sh
@@ -1,11 +1,19 @@
 #! /bin/bash
 #Script to tidy to sheba results files
 DATA=/Users/ja17375/Shear_Wave_Splitting/Sheba/Results/Sheba_results_$1.temp
+OUT=/Users/ja17375/Shear_Wave_Splitting/Sheba/Results/Sheba_$1_results.sdb
 #Header is saved in file /Users/ja17375/Shear_Wave_Splitting/Sheba/final_header
-#cat /Users/ja17375/Shear_Wave_Splitting/Sheba/final_header >! $OUT 
+#cat /Users/ja17375/Shear_Wave_Splitting/Sheba/final_header >! $OUT
 
 for f in /Users/ja17375/Shear_Wave_Splitting/Sheba/Results/*_$1_sheba_results.txt
 do
-tail -n +2  -q $f` > $DATA
+#echo $f
+tail -n +2 -q $f | awk '{print $0}' >> $DATA
 
 done
+echo 'DATE TIME STAT STLA STLO EVLA EVLO EVDP DIST AZI BAZ FAST DFAST TLAG DTLAG SPOL DSPOL WBEG WEND EIGORIG EIGCORR Q SNR NDF' > hdr
+
+cat hdr $DATA > $OUT
+
+rm $DATA
+rm hdr
--- tidyup_by_stat.sh
+++ tidyup_by_stat.sh
@@ -5,34 +5,30 @@
 # $1 = Station to tidy up sheba results
 # $2 = Phase the has been measured
 # $3 = Path to Sheba directory
+STAT=$1
+PHASE=$2
 SPATH=$3
-echo 'DATE  TIME    EVLA    EVLO    STLA    STLO    EVDP    DIST     AZI     BAZ    FAST   DFAST    TLAG   DTLAG    SPOL   DSPOL    WBEG    WEND  STAT' > head1
-echo 'EIGORIG EIGCORR      Q    SNR    NDF' > head2
+echo 'DATE  TIME  STAT  EVLA    EVLO    STLA    STLO    EVDP    DIST     AZI     BAZ    FAST   DFAST    TLAG   DTLAG    SPOL   DSPOL    WBEG    WEND   EIGORIG    EIGCORR    Q    SNR    NDF' > hdr
 #rm -f $3/Stat_$2_results_list.txt
 
 #touch $3/Stat_$2_results_list.txt
 
-if [ ${#1} -eq 3 ]
-then
-	tail -n +2  -q $SPATH/Sheba/SAC/$1/$2/*.final_result | cut -c-142,144-147 | cat head1 -  > temp1
-elif [ ${#1} -eq 4 ]
-then
-	tail -n +2 -q $SPATH/Sheba/SAC/$1/$2/*.final_result |cut -c-142,144-148 | cat head1 - > temp1
-fi
 
-tail -1 -q $SPATH/Sheba/SAC/$1/$2/$1_$2_*_sheba.stats | cut -c13-50 | cat head2 - > temp2
+tail -n +2 -q $SPATH/Sheba/SAC/$STAT/$PHASE/*.final_result |cut -c-142 | cat  > temp1
+
+tail -1 -q $SPATH/Sheba/SAC/$STAT/$PHASE/$STAT_$PHASE_*_sheba.stats | cut -c13-50 | cat  > temp2
 
 
-paste temp1 temp2 > temp_result
+
 #cat temp_result
-#cat temp_result > $1_$2_sheba_results.txt
 
+paste temp1 temp2 |\
+	awk -v stat="$1" '{print $1,$2,stat,$5,$6,$3,$4,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23}' > temp_result
 
-awk '{print $1,$2,$19,$5,$6,$3,$4,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$20,$21,$22,$23,$24}' temp_result  > $SPATH/Sheba/Results/$1_$2_sheba_results.txt
-
+cat hdr temp_result > $SPATH/Sheba/Results/$1_$2_sheba_results.txt
 #echo $SPATH/Sheba/Results/$1_$2_sheba_results.txt > int
 #cat $3/Stat_$2_results_list.txt int  > $3/Stat_$2_results_list.txt
 
 echo $1 $2
 ## Now to "tidy up" my temp variables
-rm temp1 temp2 temp_result head1 head2
+rm temp1 temp2 temp_result
