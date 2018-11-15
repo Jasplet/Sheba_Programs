576e8110772339d315c3b1ba9258d878f2f52e66
Joseph Asplet <joseph.asplet@bristol.ac.uk>
2018-11-15 12:04:45 +0000

    Removed unessecary export PATH line. TauP is alreayd set to path in .bash_rc

--- pierce.sh
+++ pierce.sh
@@ -1,35 +1,42 @@
 #! /bin/bash
 ## First lets make sure that TauP is in our PATH
-export PATH=/Users/ja17375/Ext_programs/TauP/bin:$PATH
+#export PATH=/Users/ja17375/Ext_programs/TauP/bin:$PATH
 
 ## Args
 #PHASE=$1 #Phase you want the pierce points for
 
-INFILE=${1%.sdb} # SDB file that you want the pierce point format
-DEPTH=2691 # [km] depth we want the pierece point (~ 200km above CMB at 2891km)
+INFILE=${1} # SDB file that you want the pierce point format
+OUTFILE=${1%.*} # Removes infile extension so we can replace it with .pp
+DEPTH=2889 # [km] depth we want the pierece point
+echo $INFILE
+echo $OUTFILE.pp
+rm $OUTFILE.pp
+#echo $INFILE
+rm SKS_tmp
+rm SKS_tmp
 rm SKS
 rm SKKS
-rm $INFILE.mspp
-rm SKS_tmp
-rm SKKS_tmp
 
 function call_taup_pierce {
-  #echo $3
-  taup_pierce -sta $1 $2 -evt $3 $4 -h $5 -ph $6 -pierce 2889 --nodiscon
+  #echo $1
+  taup_pierce -evt $1 $2 -sta $3 $4 -h $5 -ph $6 -pierce 2889 --nodiscon
 }
 
-echo 'dist time depth  lat_SKS lon_SKS' > SKS_hdr
+echo 'dist time depth lat_SKS lon_SKS' > SKS_hdr
 echo 'dist time depth lat_SKKS lon_SKKS' > SKKS_hdr
 
 i=1
+echo 'STAT EVLA EVLO STLA STLO EVDP' | awk '{print $3,$4,$5,$6,$7,$8}'
 while read file; do
     #echo $i
     if [ $i -gt 1 ]
     then
-
+    echo $file | awk '{print $3,$4,$5,$6,$7,$8}'
     call_taup_pierce $(echo $file |  awk  '{print $4, $5, $6, $7, $8}' ) SKS | grep 2889 | tail -1 > SKS_tmp
-    #echo SKKS
+
     call_taup_pierce $(echo $file |  awk  '{print $4, $5, $6, $7, $8}' ) SKKS | grep 2889 | head -3 | tail -1 > SKKS_tmp
+    # For a depth of 2800 we get slight less outputs than a depth of 2889 therefore would use head -2 instead of head -3 we use when pierce depth is 2889
+    cat SKKS_tmp
     else
     echo 'Skipping Header Line'
     fi
@@ -38,20 +45,20 @@ while read file; do
     cat SKS_tmp >> SKS
     cat SKKS_tmp >> SKKS
 
-    awk 'BEGIN{print ">"} {print $5, $4}' SKS_tmp >> $INFILE.mspp
-    awk '{print $5, $4}' SKKS_tmp >> $INFILE.mspp
+    # awk 'BEGIN{print ">"} {print $5, $4}' SKS_tmp >> $INFILE.mspp
+    # awk '{print $5, $4}' SKKS_tmp >> $INFILE.mspp
 
-done < $INFILE.sdb
+done < $INFILE
 
-echo $i
+
 
 
 cat SKS_hdr SKS > SKS_pierce.pp
 cat SKKS_hdr SKKS > SKKS_pierce.pp
 
-
-paste SKS_pierce.pp SKKS_pierce.pp  | awk '{print $5, $4, $10, $9}' > $INFILE.pp
+echo $OUTFILE.pp
+paste SKS_pierce.pp SKKS_pierce.pp  | awk '{print $5, $4, $10, $9}' > $OUTFILE.pp
 #awk '{print $1,$2,$3,$4,$5,$6,$7,$8,$12,$13,$14,$15,$25,$26,$27,$28}' $INFILE > tmp
 #paste tmp SKS_SKKS_tmp > SKS_SKKS_pairs.pp
 
-rm tmp SKS_SKKS_tmp
+rm SKS_hdr SKKS_hdr
